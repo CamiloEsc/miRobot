d9ae4be94f644d378a5beacdb0b24b71249bb373
Inspiron <Inspiron@DESKTOP-D0H97ON>
2020-01-05 17:27:30 -0500

    update

--- miRobot/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/camilo/miRobot/src
-# Build directory: /home/camilo/miRobot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("miRobot")
