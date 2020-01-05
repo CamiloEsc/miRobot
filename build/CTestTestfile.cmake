9f55545f6db7966b375462428674135a242ba988
Inspiron <Inspiron@DESKTOP-D0H97ON>
2020-01-05 17:27:17 -0500

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/camilo/miRobot/src
+# Build directory: /home/camilo/miRobot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("miRobot")
---
+++ build/miRobot/CMakeFiles/miRobot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miRobot.dir/src2/miRobot.cpp.o"
+  "CMakeFiles/miRobot.dir/src2/listener.cpp.o"
+  "CMakeFiles/miRobot.dir/src2/comandos.cpp.o"
+  "/home/camilo/miRobot/devel/lib/libmiRobot.pdb"
+  "/home/camilo/miRobot/devel/lib/libmiRobot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
